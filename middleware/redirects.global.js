7743c825b94b8298eb9ce56ba76bf425a76fd974
smriazati <sarahriazati@gmail.com>
2023-03-06 21:29:30 -0500

    add redirects from old site paths

--- middleware/redirects.global.js
+++ middleware/redirects.global.js
@@ -1,12 +1,12 @@
 export default defineNuxtRouteMiddleware((to, from) => {
-  console.log('to', to, 'from', from)
-  if (to.path === '/videoanimation') {
+  // console.log('to', to, 'from', from)
+  if (to.path.includes('/videoanimation')) {
     return navigateTo('/work?category=Video')
   }
-  if (to.path === '/documentary') {
+  if (to.path.includes('/documentary')) {
     return navigateTo('/work?category=Video')
   }
-  if (to.path === '/web') {
+  if (to.path.includes('/web')) {
     return navigateTo('/work?category=Web')
   }
 })
